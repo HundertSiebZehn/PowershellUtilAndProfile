e95fc65839018e18f93249ba7112b39af55bf883
Marcel Benthin <marcel.benthin@googlemail.com>
2021-03-27 13:58:46 +0100

    remove debug write-host

--- CustomProfile.ps1
+++ CustomProfile.ps1
@@ -3,7 +3,6 @@ function Load-Module([string] $name) {
     if (-not [System.IO.File]::Exists($path)) {
         if (-not (Get-Module -ListAvailable -Name $name)) {
             # $name is not installed
-            Write-Host "file is not installed"
             Install-Module $name -Force
         }
         Import-Module $name
@@ -14,11 +13,10 @@ function Load-Module([string] $name) {
 }
 
 Load-Module PSWriteColor
+Load-Module posh-docker
+Load-Module posh-git
 Load-Module ./utils.psm1
 Load-Module ./sudo.psm1
-Load-Module DockerCompletion
-Load-Module DockerComposeCompletion
-Load-Module posh-git
 
 function prompt {
     if ($(git status 2>&1 | Out-Null; $LASTEXITCODE)) {
