546cfa9fc1581e7d40047502a0c09d0ab9e214d0
marcel.benthin <iqG2b6fr4UC9WYj85gjFNfZFYwYZjFRVjonQx9vA>
2022-01-21 16:41:40 +0100

    WIP: support git-sub-repos

--- utils.psm1
+++ utils.psm1
@@ -26,17 +26,49 @@ function Format-PromptPath([string] $path) {
 }
 
 function Get-CustomGitPrompt {
-    if ($(git status 2>&1 | Out-Null; $LASTEXITCODE)) {
+    if (-not $(Test-Git $(Get-PromptPath))) {
         # not a git repo … return default prompt
         return Get-CustomPrompt
     }
     $rootPath = ($(git rev-parse --show-toplevel).replace("/", "\"))
+    $root = Get-GitRootName $rootPath
     $path = ($(Get-PromptPath).replace($rootPath, ""))
-    $root = $(Split-Path -Leaf $rootPath)
     if ($path -eq "") {
-        return Format-PromptPath(" $root")
+        return Format-PromptPath($root)
     }
-    return Format-PromptPath(" $root\" + $(Get-ShortPath($path)))
+    return Format-PromptPath($root + "\" + $(Get-ShortPath($path)))
+}
+
+function Test-Git {
+    Param (
+        [Parameter(Mandatory=$True)]
+        [String]$Path
+    )
+    if ([System.IO.Path]::IsPathRooted($Path)) {
+        # absolute path
+        $Path = $(Join-Path -ErrorAction SilentlyContinue $Path "/.git" -Resolve)
+    } else {
+        $Path = $(Join-Path -ErrorAction SilentlyContinue $PWD $Path "/.git" -Resolve)
+    }
+    return [System.Boolean] $(Test-Path -ErrorAction SilentlyContinue -Path $Path)
+}
+
+function Get-GitRootName([string] $path) {
+    if (-not $(Test-Git $path)) {
+        return ""
+    }
+    Write-Host $path
+    $rootPath = $(git -C $path rev-parse --show-toplevel).replace("/", "\")
+    Write-Host $rootPath
+    $root = $(Split-Path -Leaf $rootPath)
+    Write-Host $root
+    $parentPath = $(Join-Path -ErrorAction SilentlyContinue $rootPath ".." -Resolve)
+    Write-Host $parentPath
+    if ($(Test-Git $parentPath)) {
+        Write-Host "recursive call"
+        return $(Get-GitRootName $parentPath) + " »  $root"
+    }
+    return " $root"
 }
 
 function Get-CustomPrompt {
@@ -46,3 +78,5 @@ function Get-CustomPrompt {
 Export-ModuleMember -Function Get-ShortPath
 Export-ModuleMember -Function Get-CustomGitPrompt
 Export-ModuleMember -Function Get-CustomPrompt
+Export-ModuleMember -Function Test-Git
+Export-ModuleMember -Function Get-GitRootName
